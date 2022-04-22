2ea5ce333ea67c73899f0889a9688c410abac74e
Trisha Moitra <trishamoitra.217@gmail.com>
2022-04-23 01:05:40 +0530

    Added solution - LeetHub

--- Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.cpp
+++ Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.cpp
@@ -106,20 +106,41 @@ struct Node
 class Solution {
 public:
     
-    bool isLeaf(Node * root) {
-  return !root -> left && !root -> right;
-}
-
-void addLeftBoundary(Node * root, vector < int > & res) {
-  Node * cur = root -> left;
-  while (cur) {
-    if (!isLeaf(cur)) res.push_back(cur -> data);
-    if (cur -> left) cur = cur -> left;
-    else cur = cur -> right;
-  }
-}
-
-void addRightBoundary(Node * root, vector < int > & res) {
+    void addLeft(Node *root, vector<int> &res){
+        Node* cur = root->left;
+        while(cur){
+            if(!isLeaf(cur)){
+                res.push_back(cur->data);
+            }
+            if(cur->left){
+                cur = cur->left;
+            }
+            else{
+                cur = cur->right;
+            }
+        }
+    }
+    
+    // void addRight(Node *root, vector<int> &res){
+    //     vector<int> temp;
+    //     Node* cur = root->right;
+    //     while(cur){
+    //         if(!isLeaf(cur)){
+    //             temp.push_back(cur->data);
+    //         }
+    //         if(cur->right){
+    //             cur = cur->right;
+    //         }
+    //         else{
+    //             cur = cur->left;
+    //         }
+    //         for(int i=temp.size()-1;i>=0;--i){
+    //             res.push_back(temp[i]);
+    //         }
+    //     }
+    // }
+    
+    void addRight(Node * root, vector < int > & res) {
   Node * cur = root -> right;
   vector < int > tmp;
   while (cur) {
@@ -131,30 +152,37 @@ void addRightBoundary(Node * root, vector < int > & res) {
     res.push_back(tmp[i]);
   }
 }
-
-void addLeaves(Node * root, vector < int > & res) {
-  if (isLeaf(root)) {
-    res.push_back(root -> data);
-    return;
-  }
-  if (root -> left) addLeaves(root -> left, res);
-  if (root -> right) addLeaves(root -> right, res);
-}
+    
+    void addLeaves(Node *root, vector<int> &res){
+        if(isLeaf(root)){
+            res.push_back(root->data);
+            return;
+        }
+        if(root->left){
+            addLeaves(root->left,res);
+        }
+        if(root->right){
+            addLeaves(root->right,res);
+        }
+    }
+    
+    bool isLeaf(Node *root){
+        return !root -> left && !root -> right;
+    }
     
     vector <int> boundary(Node *root)
     {
-          vector < int > res;
-  if (!root) return res;
-
-  if (!isLeaf(root)) res.push_back(root -> data);
-
-  addLeftBoundary(root, res);
-
-  // add leaf nodes
-  addLeaves(root, res);
-
-  addRightBoundary(root, res);
-  return res;
+        vector<int> res;
+        if(!root){
+            return res;
+        }
+        if(!isLeaf(root)){
+            res.push_back(root->data);
+        }
+        addLeft(root,res);
+        addLeaves(root,res);
+        addRight(root,res);
+        return res;
         //Your code here
     }
 };
