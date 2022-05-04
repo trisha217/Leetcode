84f865ccac8b7ecde1d8dc4a4fbe1a1179d78635
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-04 19:36:47 +0530

    Time: 129 ms (26.84%), Space: 15.9 MB (76.11%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -6,23 +6,15 @@ public:
     }
     int maximumUnits(vector<vector<int>>& vec, int truckSize) {
         sort(vec.begin(), vec.end(), comp);
-        // for(auto&it:vec){
-        //     cout<<it[0]<<it[1]<<endl;
-        // }
         int ans=0;
-        int boxes=0;
         for(auto&it:vec){
                 if(it[0]<=truckSize){
                     ans =ans + (it[0]*it[1]);
                     truckSize=truckSize-it[0];
-                    //cout<<"ans"<<ans<<endl;
-                    //cout<<"ts"<<truckSize<<endl;
                 }
                 else
                 {
                     ans = ans + (truckSize*it[1]);
-                    // cout<<"ans"<<ans<<endl;
-                    // cout<<"ts"<<truckSize<<endl;
                     break;
                 }
         }
