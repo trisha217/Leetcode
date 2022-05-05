4310dea64e830c519e3d7774d2c1ed37d669ef6b
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-05 18:25:11 +0530

    Time: 3 ms (57.86%), Space: 8.2 MB (91.12%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
+    
+    void swap(vector<int>& nums,int a, int b)
+    {
+        int temp = nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
     void sortColors(vector<int>& nums) {
-        int n1=0;
-        int n2=0;
-        int n0=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0){
-                n0++;
+        int i=0;
+        int j=nums.size()-1;
+        int k=j;
+        while(i<=j){
+            int curr = nums[j];
+            if(curr ==0){
+                swap(nums,i,j);
+                i++;
             }
-            else if(nums[i]==1){
-                n1++;
-            }
-            else{
-                n2++;
-            }
-        }
-        //cout<<n0<<n1<<n2;
-        int d1=0,d2=0,d0=0;
-        for(int i=1;i<=nums.size();i++){
-            if(d0<n0){
-                nums[i-1]=0;
-                d0++;
-                cout<<"i printed 0"<<endl;
-            }
-            else if(d1<n1){
-                nums[i-1]=1;
-                d1++;
-                cout<<"i printed 1"<<endl;
-            }
-            else if(d2<n2){
-                nums[i-1]=2;
-                d2++;
-                cout<<"i printed 2"<<endl;
+            else if(curr==2){
+                swap(nums,j,k);
+                j--;
+                k--;
             }
             else{
-                
+                j--;
             }
         }
+        
     }
 };
