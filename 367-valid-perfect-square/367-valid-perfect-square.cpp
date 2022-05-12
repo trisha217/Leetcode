91e91f10079d0564abd5f6e53d794ba509e27a5e
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-12 19:08:00 +0530

    Time: 2 ms (38.11%), Space: 5.8 MB (87.52%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int n) {
+        long long i;
+        for(i=1;i*i<n;i++){
+            
+        }
+        if(i*i==n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
