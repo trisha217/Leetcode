66f36642926236facf22a8ec7788afadc8c3d1e7
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-12 22:19:13 +0530

    Time: 27 ms (10.97%), Space: 5.9 MB (26.73%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int i;
+        for(i =1;i*i<=x;i++){
+            
+        }
+        long long int s = i-1;
+        // if(s*s==n){
+        //     return s;
+        // }
+        return s;
+        
+    }
+};
