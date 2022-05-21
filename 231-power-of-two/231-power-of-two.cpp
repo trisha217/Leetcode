0a319f8c8ee340448441dabf87df86fcbb1e6380
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-21 22:43:49 +0530

    Time: 3 ms (53.88%), Space: 6 MB (25.77%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n>0 && !(n & n-1)){
-            return true;
+        if(n==0){
+            return 0;
         }
-        else{
-            return false;
+        if(n<0){
+            return 0;
         }
+        if((n&(n-1))==0){
+            return 1;
+        }
+        return 0;
     }
 };
