cde9b111f7aee311f19959a181edcfe86da7d1c9
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-23 21:32:10 +0530

    Time: 4 ms (28.68%), Space: 6.4 MB (51.75%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        int len = s.length();
-        for(int i=0;i<len;i++)
-        {
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-            {
-                st.push(s[i]);
+        if(s.size() <= 1){
+            return false;
+        }
+        stack<char> stack;
+        for(int i = 0; i < s.size(); i++){
+            if(stack.empty() == true){
+                stack.push(s[i]);
+            }else{
+                if(stack.top() == '(' && s[i] == ')'){
+                    stack.pop();
+                    continue;
+                }
+                if(stack.top() == '[' && s[i] == ']'){
+                    stack.pop();
+                    continue;
+                }
+                if(stack.top() == '{' && s[i] == '}'){
+                    stack.pop();
+                    continue;
+                }
+                stack.push(s[i]);
                 continue;
             }
-            if (st.empty())
-                return false;
-            char x;
-            switch (s[i]) 
-            {
-                case ')':
-                    x = st.top();
-                    st.pop();
-                    if (x == '{' || x == '[')
-                        return false;
-                    break;
- 
-                case '}':
-                    x = st.top();
-                    st.pop();
-                    if (x == '(' || x == '[')
-                        return false;
-                        break;
- 
-                case ']':
-                    x = st.top();
-                    st.pop();
-                    if (x == '(' || x == '{')
-                        return false;
-                        break;
-            }
         }
-        if(st.empty())
-        {
-            return true;
-        }
-        return false;        
+    return stack.empty()==true;
     }
 };
