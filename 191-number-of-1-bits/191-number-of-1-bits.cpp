06563fd6fd683eedcfee709e2598952d908a7544
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-24 22:39:14 +0530

    Time: 4 ms (36.13%), Space: 5.9 MB (79.19%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
-    
-    while(n!=0){
-        if(n&1){
+        while(n){
             count++;
+            n &= n-1;
         }
-        
-        n=n>>1;
-    }
-    
-    return count;
-
-        
+        return count;
     }
 };
