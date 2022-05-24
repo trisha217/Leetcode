b1bffa0fa6df42deaa36c4cc3c64b11d6d546234
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-24 19:07:56 +0530

    Time: 2 ms (74.56%), Space: 5.9 MB (87.57%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if(n ==1){
+            return 0;
+        }
+        int ans =0;
+        for(int i=2;i*i<=n;){
+            if(n%i==0){
+                ans = ans +i;
+                n = n/i;
+            }
+            else{
+                i++;
+            }
+        }
+        if(n!=1){
+            ans = ans +n;
+        }
+        return ans;
+    }
+};
