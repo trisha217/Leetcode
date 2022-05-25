6bd13a0bdbe0e489791d130a2ade42115b8c5a41
Trisha Moitra <trishamoitra.217@gmail.com>
2022-05-26 02:09:17 +0530

    Time: 8 ms (18.63%), Space: 6.7 MB (67.84%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) 
-    {
-      for(int i=0;i<s.size();i++)
-		t[i+1]+=t[i]-s[i]; //Passing the diff: (t[i]-s[i]) to t[i+1]
-      return t[t.size()-1]; //The diff will be carried over to the last element eventually
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        int m = s.size();
+        int i = 0;
+        while(i < m)
+        {
+            if(s[i] != t[i])
+                return t[i];
+            i++;
+        }
+        return t[i];
     }
 };
