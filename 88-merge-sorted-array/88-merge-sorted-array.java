693e30cc774d4f80bfb8416ca38401b2b35f05a9
Trisha Moitra <trishamoitra.217@gmail.com>
2022-06-07 23:39:30 +0530

    Time: 0 ms (100.00%), Space: 42 MB (96.96%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,28 +1,20 @@
 class Solution {
-    public void merge(int[] arr1, int m, int[] arr2, int n)
-    {
-        int i,j,k;
-        if(m!=0 && n!=0)
-        {
-            for (i = 0; i < m; i++)
-            {
-                if (arr1[i] > arr2[0]) {
-                    int temp = arr1[i];
-                    arr1[i] = arr2[0];
-                    arr2[0] = temp;
-                }
-                Arrays.sort(arr2);
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m + n <= 0) return;
+        if (m > 0 && n > 0) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[m + n - 1] = nums1[m - 1];
+                merge(nums1, m - 1, nums2, n);
+            } else {
+                nums1[m + n - 1] = nums2[n - 1];
+                merge(nums1, m, nums2, n - 1);
             }
-            for(j=m, i=0;j<m+n && i<n;j++){
-                arr1[j] = arr2[i];
-                i++;            
-            }
-        }
-        else
-        {
-            for(j=0,i=0;j<n;j++,i++){
-                arr1[i]=arr2[j];
-            }
+        } else if (m > 0) {
+            nums1[m + n - 1] = nums1[m - 1];
+            merge(nums1, m - 1, nums2, n);
+        } else if (n > 0) {
+            nums1[m + n - 1] = nums2[n - 1];
+            merge(nums1, m, nums2, n - 1);
         }
     }
 }
