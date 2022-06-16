ae82e271c57799e25b2868748ebb3196bd643253
Trisha Moitra <trishamoitra.217@gmail.com>
2022-06-16 23:55:28 +0530

    Time: 36 ms (47.66%), Space: 11.4 MB (54.13%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    string removeDuplicates(string a) {
-        // stack<char> st;
-        // for(auto i:s){
-        //     if(st.empty()) 
-        //         st.push(i);
-        //     else if(st.top()==i)
-        //         st.pop();
-        //     else
-        //         st.push(i);
-        // }
-        // string sn="";
-        // while(!st.empty()){
-        //     sn = sn + st.top();
-        //     st.pop();
-        // }
-        // reverse(sn.begin(),sn.end());
-        // return sn;
-        
+    string removeDuplicates(string s) {
         stack<char> st;
-	string ans="";
-	for(auto curr:a) {
-		if(st.empty()) st.push(curr);
-		else if(st.top() == curr) st.pop();
-		else st.push(curr);
-	}
-
-	while(!st.empty()) {
-		ans += st.top();
-		st.pop();
-	}
-
-	reverse(ans.begin(), ans.end());
-
-	return ans;
+        string sn="";
+        for(auto i:s){
+            if(st.empty()) st.push(i);
+            else if(st.top()==i) st.pop();
+            else st.push(i);
+        }
+        while(!st.empty()){
+            sn+= st.top();
+            st.pop();
+        }
+        reverse(sn.begin(),sn.end());
+        return sn;
+        
     }
 };
