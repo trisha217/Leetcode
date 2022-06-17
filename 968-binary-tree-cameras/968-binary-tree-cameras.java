d65d6e35a2d8f840ff497ebd092c5a958a788315
Trisha Moitra <trishamoitra.217@gmail.com>
2022-06-17 23:11:23 +0530

    Time: 1 ms (76.65%), Space: 43.3 MB (62.72%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -37,23 +37,3 @@ class Solution {
         return -1;
     }
 }
-
-// class Solution {
-//     private int numOfCameras = 0;
-    
-//     public int minCameraCover(TreeNode root) {
-//         return dfs(root) == -1 ? numOfCameras + 1 : numOfCameras;
-//     }
-//     private int dfs(TreeNode root) {
-//         if (root == null) return 0;
-//         int left = dfs(root.left);
-//         int right = dfs(root.right);
-//         if (left == -1 || right == -1) {
-//             numOfCameras++;
-//             return 1; 
-//         }
-//         if (left == 1 || right == 1)
-//             return 0; 
-//         return -1;
-//     }
-// }
