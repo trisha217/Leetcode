f4770e85fe9a1a2058f910a1ad2e4dcbdd78b370
Trisha Moitra <trishamoitra.217@gmail.com>
2022-06-18 19:34:19 +0530

    Time: 49 ms (77.72%), Space: 68.7 MB (39.70%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -1,68 +1,82 @@
+/*
+	Every Node has a Array of 26 size to mark which chars are its children , also it has
+	a boolean variable isEnd to mark end of any word.
+
+	insert(): Point to root using curr pointer, now start a loop and extract each char from the
+	word to be inserted in TRIE,  for every char check if it is present as child of
+	curr node(for first char curr node will be root node).if no , then insert the char as the 
+	child of curr and move the cur pointer to that child, so that next time we can insert
+	the char as its child. Do this till the end char of word and outside the for loop
+	mark the isEnd property of curr node (i.e the last char of our word) as true;
+
+	search() &  startsWith() : keep checking char by char and if you don't found your char
+	at any point return false, else at the end check for isEnd variable (in search() fun only)
+	and return true(in startsWith () only).
+
+	Because search function means that word should be completely present as a whole
+	word and not as a part(substring ) of any other word thats why we are checking the
+	isEnd flag , while startsWith() only want us to check that prefix is present in the trie or 
+	not, it doesn't bother wheter we have it as a complete word or as a substring of any 
+	other word.
+*/
 class Trie {
-    /** Initialize your data structure here. */
-    boolean isEnd;
-    Trie[] link;
+    
+    class Node {
+        Node [] childs;
+        boolean isEnd;
+        
+        Node(){
+            childs = new Node[26];
+            isEnd = false;
+        }
+    }
+    
+    final private Node root;
+    
     public Trie() {
-        isEnd=false;
-        link=new Trie[26];
+        root = new Node();
     }
     
+ 
     public void insert(String word) {
-        int i=0;
-            char[] ch=word.toCharArray();
-            Trie t=this;
-            while(t!=null && i<ch.length && t.link[ch[i]-'a']!=null){
-                t=t.link[ch[i]-'a'];
-                i++;
-            }
-            if(i==ch.length){ 
-                t.isEnd=true;
-                return;
-            }
+        Node curr = root;
         
-            for(;i<ch.length;i++){
-                Trie temp=new Trie();
-                
-                t.link[ch[i]-'a']=temp;
-                t=temp;
-            } 
+        for(int i = 0;i<word.length();i++){
+            char ch = word.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null){
+                curr.childs[ch - 'a'] = new Node();
+            }
+            curr = curr.childs[ch - 'a'];
+        }
         
-           t.isEnd=true;
+        curr.isEnd = true;
     }
     
+ 
     public boolean search(String word) {
-        int i=0;
-        char[] ch=word.toCharArray();
-        Trie t=this;
-        while(t!=null && i<ch.length && t.link[ch[i]-'a']!=null){
-                t=t.link[ch[i]-'a'];
-                i++;
-            }
-         if(i==ch.length && t.isEnd){
-                return true;
-            }
-        return false;
+        Node curr = root;
+        
+        for(int i = 0;i<word.length();i++){
+            char ch = word.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null) return false;
+            curr = curr.childs[ch - 'a'];
+        }
+        return curr.isEnd;
     }
     
+  
     public boolean startsWith(String prefix) {
-        int i=0;
-        char[] ch=prefix.toCharArray();
-        Trie t=this;
-        while(t!=null && i<ch.length && t.link[ch[i]-'a']!=null){
-                t=t.link[ch[i]-'a'];
-                i++;
-            }
-         if(i==ch.length){
-                return true;
-            }
-        return false;
+          Node curr = root;
+        
+        for(int i = 0;i<prefix.length();i++){
+            char ch = prefix.charAt(i);
+            
+            if(curr.childs[ch - 'a'] == null) return false;
+            curr = curr.childs[ch - 'a'];
+        }
+        
+        return true;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
