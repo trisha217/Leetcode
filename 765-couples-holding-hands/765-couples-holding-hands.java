6920d9c400a47c30c62457fb058d6ae609b88c41
Trisha Moitra <trishamoitra.217@gmail.com>
2022-06-22 01:59:08 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (16.67%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.java
+++ 765-couples-holding-hands/765-couples-holding-hands.java
@@ -1,25 +1,18 @@
 class Solution {
-       public static void swap(int[] row, int[] pos, int x, int y) {
-        int temp = row[x];
-        pos[temp] = y;
-        pos[row[y]] = x;
-        row[x] = row[y];
-        row[y] = temp;
-    }
     public int minSwapsCouples(int[] row) {
-        int n = row.length;
-        int[] pos = new int[n];
-        for (int i = 0; i < n; i++) {
-            pos[row[i]] = i;
-        }
-        int count = 0;
-        for (int i = 0; i < n; i += 2) {
-            int j = row[i] % 2 == 0 ? row[i] + 1 : row[i] - 1;
-            if (row[i + 1] != j) {
-                swap(row, pos, i + 1, pos[j]);
-                count++;
-            }
-        }
-        return count;
+        int[] pos = new int[row.length];
+	for (int i = 0; i < row.length; i++)
+		pos[row[i]] = i;
+
+	int res = 0;
+	for (int i = 0; i < row.length; i += 2) {
+		int j = row[i] % 2 == 0 ? (row[i] + 1) : (row[i] - 1); 
+		if (row[i + 1] != j) {
+			row[pos[j]] = row[i + 1];
+			pos[row[i + 1]] = pos[j];
+			res++;
+		}
+	}
+	return res;
     }
 }
