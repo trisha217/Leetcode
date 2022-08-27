d25fca6673e6a97bf8bd9e0b12b49f286bce4faf
Trisha Moitra <trishamoitra.217@gmail.com>
2022-08-27 11:14:09 +0530

    Time: 36 ms (57.85%), Space: 16.1 MB (6.00%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(int node, vector<int> adjLs[], int vis[]) {
+        // mark the more as visited
+        vis[node] = 1; 
+        for(auto it: adjLs[node]) {
+            if(!vis[it]) {
+                dfs(it, adjLs, vis); 
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>> adj) {
+        int V = adj.size();
+        vector<int> adjLs[V]; 
+        
+        // to change adjacency matrix to list 
+        for(int i = 0;i<V;i++) {
+            for(int j = 0;j<V;j++) {
+                // self nodes are not considered
+                if(adj[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
+                }
+            }
+        }
+        int vis[V]; 
+        memset(vis, 0, sizeof(vis));
+        int cnt = 0; 
+        for(int i = 0;i<V;i++) {
+            // if the node is not visited
+            if(!vis[i]) {
+                // counter to count the number of provinces 
+                cnt++;
+               dfs(i, adjLs, vis); 
+            }
+        }
+        return cnt; 
+        
+    }
+};
