7108ab1e9d24a04834bc2131b39458028ad3a5f3
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-02 00:29:31 +0530

    Time: 754 ms (17.54%), Space: 10.6 MB (44.59%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -15,20 +15,41 @@ class Solution {
     //     }
     //     return dp[ind][prev_ind+1] = max(take,not_take);
     // }
-    int lengthOfLIS(vector<int> nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-        for(int ind = n-1; ind>=0; ind --){
-            for (int prev_ind = ind-1; prev_ind >=-1; prev_ind --){
-                int take= 0;
-                int not_take = 0 + dp[ind+1][prev_ind +1];
-                if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
-                    take = 1+dp[ind+1][ind+1];
-                }
-                dp[ind][prev_ind+1] = max(take,not_take);
+//     int lengthOfLIS(vector<int> nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+//         for(int ind = n-1; ind>=0; ind --){
+//             for (int prev_ind = ind-1; prev_ind >=-1; prev_ind --){
+//                 int take= 0;
+//                 int not_take = 0 + dp[ind+1][prev_ind +1];
+//                 if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
+//                     take = 1+dp[ind+1][ind+1];
+//                 }
+//                 dp[ind][prev_ind+1] = max(take,not_take);
+//             }
+//         }
+        
+//         return dp[0][0];
+//     }
+    int lengthOfLIS(vector<int> arr) {
+        int n = arr.size();
+    vector<int> dp(n,1);
+    
+    for(int ind=0; ind<=n-1; ind++){
+        for(int prev_index = 0; prev_index <=ind-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[ind]){
+                dp[ind] = max(dp[ind], 1 + dp[prev_index]);
             }
         }
-        
-        return dp[0][0];
+    }
+    
+    int ans = -1;
+    
+    for(int i=0; i<=n-1; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    return ans;
     }
 };
