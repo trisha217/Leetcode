0aa528906f0f5b2941cb140d0d152628a0240349
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-03 02:32:39 +0530

    Time: 118 ms (91.84%), Space: 59.8 MB (16.52%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -14,3 +14,16 @@ public:
         return p-1;
     }
 };
+
+//O(log n) time complexity
+// int peakIndexInMountainArray(vector<int>& arr) {
+//         int low = 0, high = arr.size()-1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] < arr[mid+1])
+//                 low = mid+1;
+//             else
+//                 high = mid-1;
+//         }
+//         return low;
+//     }
