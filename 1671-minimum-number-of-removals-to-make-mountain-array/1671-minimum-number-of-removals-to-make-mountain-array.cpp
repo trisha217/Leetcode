54ab6e34e32324ae27165396f09d2de1892bcfc0
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-03 01:21:41 +0530

    Time: 395 ms (58.60%), Space: 12.4 MB (20.94%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp1(n,1);
+        vector<int> dp2(n,1);
+        vector<int> bitonic(n);
+        //int maxi1=1;
+        for(int ind=0;ind<=n-1;ind++){
+            for(int prev =0;prev<=ind-1;prev++){
+                if(nums[prev]<nums[ind] && dp1[ind]<1+dp1[prev]){
+                    dp1[ind] =1 +dp1[prev];
+                }
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     cout<<dp1[i]; 
+        // }
+        // cout<<endl;
+        //int maxi2=1;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev =n-1;prev>ind;prev--){
+                if(nums[prev]<nums[ind] && dp2[ind]<1+dp2[prev]){
+                    dp2[ind] =1 + dp2[prev];
+                }
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     cout<<dp2[i]; 
+        // }
+        // cout<<endl;
+        int maxi=INT_MIN;
+	    for(int i=0;i<n;i++){
+            if(dp1[i]==1 or dp2[i]==1)
+                continue;
+	        maxi=max(maxi,dp1[i]+dp2[i]-1);
+	    }
+        return nums.size()-maxi;
+    }
+};
