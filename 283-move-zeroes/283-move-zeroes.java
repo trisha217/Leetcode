d06a8e6f9b02099e3b4bac7fa6b7f4373fefb11c
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-02 15:51:27 +0530

    Time: 3 ms (38.79%), Space: 54.8 MB (53.00%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         
         int swap=0;
         
-        for(int i=0, j=0; i<nums.length-1 && j!=nums.length;j++) {
+        for(int i=0, j=0; j<nums.length;j++) {
             if(nums[j]!=0) {
                 swap=nums[i];
                 nums[i]=nums[j];
