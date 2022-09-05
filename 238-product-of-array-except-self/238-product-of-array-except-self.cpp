4779a339df49e2fad25fc7094700dd9d807901cf
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-05 23:09:25 +0530

    Time: 40 ms (49.67%), Space: 24.6 MB (48.27%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> left,ans;
-        int product=1;
-        int n=nums.size(); 
-        vector<int> right(n,0);
-        for(int i=0;i<n;i++)
-        {
-            left.push_back(product);
-            product *= nums[i];
+        int n = nums.size();
+        vector<int> pre(n,1);
+        vector<int> suf(n,1);
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1] * nums[i-1];
         }
-        for(int i=0;i<left.size();i++)
-        {
-            cout<<left[i]<<" ";
+        for(int j=n-2;j>=0;j--){
+            suf[j] = suf[j+1] * nums[j+1];
         }
-        cout<<endl;
-        product =1;
-        for(int i=n-1;i>=0;i--)
-        {
-            right[i] = product;
-            product *= nums[i];
+        for(int i=0;i<n;i++){
+            pre[i] = pre[i] *suf[i];
         }
-        for(int i=0;i<right.size();i++)
-        {
-            cout<<right[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<n;i++)
-        {
-            ans.push_back(left[i] * right[i]);
-        }
-        return ans;
+        return pre;
     }
 };
