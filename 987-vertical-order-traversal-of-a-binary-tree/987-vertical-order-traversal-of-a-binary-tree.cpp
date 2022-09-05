06069d4bcc356fdfd591425ab8dbbd03665acd46
Trisha Moitra <trishamoitra.217@gmail.com>
2022-09-06 00:13:05 +0530

    Time: 15 ms (16.33%), Space: 13.7 MB (32.69%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -4,39 +4,27 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) 
-    {
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> que;
-        que.push({root,{0,0}});
-        while(!que.empty()){
-            auto p = que.front();
-            que.pop();
-            TreeNode* temp = p.first;
-            int x = p.second.first;
-            int y = p.second.second;
-            nodes[x][y].insert(temp->val);
-            if(temp->left){
-                que.push({temp->left,{x-1,y+1}});
-            }
-            if(temp->right){
-                que.push({temp->right,{x+1,y+1}});
-            }
-        }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>> mp;
         vector<vector<int>> ans;
-        for(auto it: nodes)
-        {
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            root=q.front().first;
+            int x=q.front().second.first,y=q.front().second.second;q.pop();
+            mp[x][y].insert(root->val);
+            if(root->left) q.push({root->left,{x-1,y+1}});
+            if(root->right) q.push({root->right,{x+1,y+1}});
+        }
+        for(auto x:mp){
             vector<int> col;
-            for(auto it2: it.second)
-            {
-                col.insert(col.end(),it2.second.begin(),it2.second.end());
+            for(auto y:x.second){
+                for(auto ele:y.second) col.push_back(ele);
             }
             ans.push_back(col);
         }
