cc1c01e36b4288a208e655cd42e942c8d5997c47
Trisha Moitra <trishamoitra.217@gmail.com>
2022-10-03 00:47:19 +0530

    Time: 20 ms (88.83%), Space: 6.1 MB (87.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
+    #define MOD 1000000007
+    int dp[32][1002];
+    
+    int solve(int d, int f, int target){
+        if(d==0 && target==0) return 1;
+        if(d<=0 || target<=0) return 0;
+        if(dp[d][target] != -1) return dp[d][target];
+     
+        int sum=0;
+        for(int i=1;i<=f;i++){
+            sum = (sum % MOD + solve(d-1,f,target-i)%MOD)%MOD;
+        }
+        dp[d][target] = sum;
+        return dp[d][target];
+    }
+    
     int numRollsToTarget(int d, int f, int target) {
-  vector<int> dp(target + 1);
-  dp[0] = 1;
-  for (int i = 1; i <= d; ++i) {
-    vector<int> dp1(target + 1);
-    for (int j = 1; j <= f; ++j)
-      for (auto k = j; k <= target; ++k)
-        dp1[k] = (dp1[k] + dp[k - j]) % 1000000007;
-    swap(dp, dp1);
-  }
-  return dp[target];
-}
+        memset(dp,-1,sizeof(dp));
+        return solve(d,f,target);
+    }
 };
