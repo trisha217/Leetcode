2febc86e27a85eefe12819182c81c4211fdcdab7
Trisha Moitra <trishamoitra.217@gmail.com>
2022-10-06 20:55:48 +0530

    Time: 3 ms (37.39%), Space: 6 MB (42.14%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,19 +1,32 @@
 class Solution {
-    int catalan_Number(int n)
-    {
-        int catalan[n+1];
-        catalan[0] = catalan[1] = 1;
-        
-        for(int i=2;i<=n;++i)
-        {
-            catalan[i] = 0;
-            for(int j=0;j<i;++j)
-                catalan[i] += catalan[j]*catalan[i-j-1];
-        }
-        return catalan[n];
-    }
 public:
+    // int numTrees(int n) {
+    //     if(n <= 1) return 1;
+    //     int ans = 0;
+    //     for(int i = 1; i <= n; i++) 
+    //         ans += numTrees(i-1) * numTrees(n-i);
+    //     return ans;
+    // }
     int numTrees(int n) {
-        return catalan_Number(n);
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++) 
+            for(int j = 1; j <= i; j++)
+                dp[i] += dp[j-1] * dp[i-j];
+        return dp[n];
     }
 };
+
+/*observation is
+      3                         
+    /   \
+f(2)    f(0) 
+    i = 3
+    
+                      i
+	=>              /   \ 
+         numTrees(i-1)	numTrees(n-i)
+    
+    
+*/
+
