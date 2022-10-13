32ec5c01affcc22144db16a0303b8bc749570ecf
Trisha Moitra <trishamoitra.217@gmail.com>
2022-10-14 01:03:46 +0530

    Time: 14 ms (85.95%), Space: 7.6 MB (37.95%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -8,8 +8,9 @@
  */
 class Solution {
 public:
-    void deleteNode(ListNode* node) {
+     void deleteNode(ListNode* node) {
+     ListNode * temp = node->next;
         node->val = node->next->val;
         node->next = node->next->next;
-    }
+        }
 };
