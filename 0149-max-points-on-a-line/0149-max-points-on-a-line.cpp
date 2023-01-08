69f47bcfc8e3fa73324190a96aba7082bbd14b8c
Trisha Moitra <trishamoitra.217@gmail.com>
2023-01-08 16:13:09 +0530

    Time: 31 ms (92.06%), Space: 13.5 MB (54.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,31 +1,49 @@
 class Solution {
-    double getSlope(double x1, double y1, double x2,double  y2)
-    {
-        return (y2-y1)/(x2-x1);
-    }
+
 public:
     int maxPoints(vector<vector<int>>& points) {
+        
         int n = points.size();
-        int m = 0;
-        for(int i=0; i<n; i++)
+        
+        // Basic Check
+        if(n<3)
+        return n;
+
+        //Setting Variables
+        int ans = 2;
+        double slope;
+
+        for(int i = 0 ; i<n-1 ; i++)
         {
-            unordered_map<double, int> mp;
-            int x1 = points[i][0], y1 = points[i][1];
-            int same = 0;
-            for(int j =i+1; j<n; j++)
+            unordered_map< double , int > mp;
+            for(int j = i+1 ; j<n ; j++)
             {
-                int x2 = points[j][0], y2 = points[j][1];
-                if(x1 == x2 )
-                {
-                    same++;
-                    continue;
-                }
-                double slope = getSlope(x1, y1, x2, y2);
-                mp[slope]++;
-                m = max(m, mp[slope]);
+                int x2 = points[i][0], x1 = points[j][0]; 
+                int y2 = points[i][1], y1 = points[j][1]; 
+                
+                // If Slope is Infinte We let Slope as INT_MAX because (You can think of it)
+                   if(x2 == x1)
+                   {  
+                      slope = INT_MAX;
+                       mp[slope]++;
+                   }
+                // Else we calculate slope and  increase its count
+                  else
+                   {
+                    slope = (1.0) * (y2-y1) / (x2-x1);
+                     mp[slope]++;
+
+                   }          
+                //    we store the max frequency for that slope 
+                // and + 1 is because we need to count ith coordinate also           
+                     ans = max(ans, mp[slope]+1);
+                
             }
-            m = max(m, same);
         }
-        return m+1;
+        
+
+        return ans;
     }
+
+    //Hope You Liked it , Upvote is appreciated 
 };
