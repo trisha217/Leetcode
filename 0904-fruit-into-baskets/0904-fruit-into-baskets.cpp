9ec0a235f5b493eb3a450de61e976b5640d1d460
Trisha Moitra <trishamoitra.217@gmail.com>
2023-02-07 14:03:52 +0530

    Time: 157 ms (86.38%), Space: 68 MB (87.39%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,30 +1,67 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        
-        unordered_map<int, int> mp;
-
-        int i=0, j=0, res=0;
-
-        while(j < fruits.size()) {
-
-            mp[fruits[j]]++;
-
-            if(mp.size() <= 2) {
-                res = max(res, j-i+1);
-            }
-
-            else {
-                mp[fruits[i]]--;
-                if(mp[fruits[i]] == 0) {
-                    mp.erase(fruits[i]);
+        queue<pair<int,int>>qm;
+        int t1=-2,t2=-2;
+        int mx=-1;
+        int cnt=0;
+        for(int i=0;i<fruits.size();i++){
+            if(qm.size()<2){
+                if(qm.size()==1 && qm.front().first==fruits[i] ){
+                    qm.front().second+=1;
+                    cnt++;
                 }
-                i++;
+                else{
+                    qm.push(make_pair(fruits[i],1));
+                    cnt++;
+                }
             }
-
-            j++;
+            else if(fruits[i]==qm.front().first  || fruits[i]==qm.back().first ){
+                cnt++;
+                if(fruits[i]==qm.front().first){
+                    if(fruits[i]==fruits[i-1]){
+                         qm.front().second+=1;
+                    }
+                    else{
+                         qm.front().second=1;
+                    }
+                }
+                else if(fruits[i]==qm.back().first){
+                    if(fruits[i]==fruits[i-1]){
+                         qm.back().second+=1;
+                    }
+                    else{
+                         qm.back().second=1;
+                    }
+                }  
+            }
+            else if(fruits[i]!=qm.front().first   && fruits[i]!=qm.back().first){
+                if(fruits[i-1]==qm.front().first){
+                    //remove back of qm
+                    cnt=1;
+                    cnt=cnt+qm.front().second;
+                    auto frnt=qm.front();
+                    qm.pop();
+                    qm.pop();
+                    qm.push(frnt);
+                }
+                else if(fruits[i-1]==qm.back().first) {
+                    //remove front of qm
+                    cnt=1;
+                    cnt=cnt+qm.back().second;
+                    qm.pop();
+                }
+                else{
+                    cnt=1;
+                }
+                qm.push(make_pair(fruits[i],1));
+                               
+            }
+            // cout<<cnt<<endl;
+            if(cnt>mx){
+                mx=cnt;
+            }
         }
-
-        return res;
+        return mx;
     }
 };
