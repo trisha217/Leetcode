d403f74292d28ca2d2cc3a619bfc1db8ef382171
Trisha Moitra <trishamoitra.217@gmail.com>
2023-03-19 15:40:07 +0530

    Time: 2783 ms (5.01%), Space: 136 MB (99.65%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,47 @@
+class WordDictionary {
+public:
+    vector<string> v;
+    WordDictionary() {}
+    
+    void addWord(string word) 
+    {
+        v.push_back(word);
+    }
+    
+    bool search(string word) 
+    {
+        int cnt = 0;
+        int n = word.size();
+        for(int i = 0; i < v.size(); i++){
+            //proceed for checking only if sizes match
+            if(n == v[i].size())
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(v[i][j] == word[j] || word[j] == '.')
+                        cnt++;
+                    
+                    else{
+                        //not a match, make default settings and run the process again
+                        cnt = 0;
+                        break;
+                    }
+                }
+                
+                if(cnt == n) 
+                    return true;
+            }
+        }
+
+
+        //all matching done but found nothing
+        return false;
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
