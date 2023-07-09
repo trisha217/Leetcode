9779e2f1db1424b9c9c4f25188dc90106642662f
Trisha Moitra <trishamoitra.217@gmail.com>
2023-07-09 21:04:16 +0530

    Time: 1342 ms (59.08%), Space: 6.9 MB (48.64%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,27 @@
+ class Solution {
+public:
+    int ans = INT_MAX ;
+    void fun(vector<int>&v , int in , vector<int>&c){
+        if(in == v.size()){
+            int y = 0;
+            for(auto x : c){
+                y = max(y , x);
+            }
+            ans = min(ans , y);
+            return ;
+        }
+
+        for(int i=0 ; i<c.size() ; i++){
+            c[i] += v[in];
+            fun(v , in+1 , c);
+            c[i] -= v[in];
+        }
+
+        return ;
+    }
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int>c(k , 0);
+        fun(cookies , 0 , c);
+        return ans ;
+    }
+};
