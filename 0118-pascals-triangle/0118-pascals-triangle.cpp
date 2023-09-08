826b85a816e256c54bc6ae3209b3a1e3501bfa0c
Trisha Moitra <trishamoitra.217@gmail.com>
2023-09-08 11:19:14 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (49.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> soln;
-        if(numRows==1) return {{1}};
-        if(numRows==2) return {{1},{1,1}};
-        soln.push_back({1});
-        soln.push_back({1,1});
-        for(int i=2;i<numRows;i++){
-            vector<int> v;
-            v.push_back(1);
-            int j=1;
-            while(j!=i){
-                v.push_back(soln[i-1][j-1]+soln[i-1][j]);
-                if(j+1==i) v.push_back(1);
-                j++;
-            }
-            soln.push_back(v);
-            
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int>> r(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);
+            r[i][0] = r[i][i] = 1;
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
         }
-        return soln;
+        
+        return r;
     }
 };
