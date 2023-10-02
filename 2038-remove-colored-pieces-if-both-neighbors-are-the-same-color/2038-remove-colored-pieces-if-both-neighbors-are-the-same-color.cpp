2fc22452c0e6b89c7b9c4ce0ec56de068675c40f
Trisha Moitra <trishamoitra.217@gmail.com>
2023-10-03 01:40:31 +0530

    Time: 30 ms (84.46%), Space: 13.7 MB (17.87%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,19 @@
+ class Solution {
+public:
+    bool winnerOfGame(string colors) 
+    {
+        int n = colors.size();
+    
+        int acount=0,bcount=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(colors[i]=='A')
+            acount+= (colors[i-1]=='A' && colors[i+1]=='A');
+            else
+            bcount+= (colors[i-1]=='B' && colors[i+1]=='B');
+        }
+        
+       return (acount>=++bcount);
+    }
+};
