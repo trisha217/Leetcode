de7a17b676b35676849da10d01722cb275313997
Trisha Moitra <trishamoitra.217@gmail.com>
2023-11-07 08:58:30 +0530

    Time: 102 ms (82.93%), Space: 83.4 MB (76.27%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -3,17 +3,13 @@ public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
         int n = dist.size();
         vector<int> temp(n);
-        // calculate time req for each monster to reach city
         for(int i=0;i<n;i++){
             temp[i]=ceil(1.0*dist[i]/speed[i]);
         }
         sort(temp.begin(),temp.end());
-        // We are sorting because we want to kill those monsters first which take less time to reach the city
         int time=1;
-        // Checking how many monsters we can kill
         for(int i=0;i<n;i++){
             if(time>temp[i]){
-                // This means current monster reached the city before we can kill it
                 return i;
             }
             time++;
