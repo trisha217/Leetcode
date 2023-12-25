d89875267c6b1ac943caf806b83e48a480f6200b
Trisha Moitra <trishamoitra.217@gmail.com>
2023-12-25 12:15:50 +0530

    Time: 1 ms (84.19%), Space: 41.8 MB (9.46%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n= s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i = 2;i<=n;i++){
+            int oneD = Integer.valueOf(s.substring(i-1,i));
+            int twoD = Integer.valueOf(s.substring(i-2,i));
+            if(oneD>=1){
+                dp[i] = dp[i] + dp[i-1];
+            }
+            //System.out.println("value of dp[i] at i " + i + " " + dp[i]);
+            if(twoD>=10 && twoD<=26){
+                dp[i] = dp[i] + dp[i-2];
+            } 
+            //System.out.println("value of dp[i] at i " + i + " " + dp[i]);
+        }
+        // for(int i=0;i<=n;i++){
+        //     System.out.print(dp[i]);
+        // }
+        // System.out.println();
+        return dp[n];
+    }
+}
