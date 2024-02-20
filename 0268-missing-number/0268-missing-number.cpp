6306a3e65c62a00d248db787a8a65dba6baeaecf
Trisha Moitra <trishamoitra.217@gmail.com>
2024-02-20 09:46:59 +0530

    Time: 7 ms (97.83%), Space: 20.6 MB (16.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -9,6 +9,7 @@ public:
         //     }
         // }
         // return n;
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         int n= nums.size();
         int sum = (n*(n+1))/2;
         return sum - accumulate(nums.begin(),nums.end(),0);
