35440fe193c209bdbddc3d64e29349a3835a7f38
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-06 17:47:42 +0530

    Time: 18 ms (84.42%), Space: 29.4 MB (46.77%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int start = 0;
-        int end = nums.size() - 1;
-        std::vector<int> res(nums.size());
-
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[start] * nums[start] > nums[end] * nums[end]) {
-                res[i] = nums[start] * nums[start];
-                start++;
-            } else {
-                res[i] = nums[end] * nums[end];
-                end--;
+        vector<int> res;
+        int n = nums.size();
+        int k;
+        for(k=0;k<n;k++){
+            if(nums[k]>=0){
+                break;
             }
         }
+        int i = k-1;
+        int j = k;
+        while (i >= 0 && j < n){
+            if(nums[i]*nums[i]<nums[j]*nums[j]){
+                res.push_back(nums[i]*nums[i]);
+                i--;
+            }
+            else{
+                res.push_back(nums[j]*nums[j]);
+                j++;
+            }
+        }
+        /*Copy the remaining elements of first half */
+        while (i >= 0) {
+            res.push_back(nums[i]*nums[i]);
+            i--;
+        }
 
+        /* Copy the remaining elements of second half */
+        while (j < n) {
+            res.push_back(nums[j]*nums[j]);
+            j++;
+        }
         return res;
     }
 };
