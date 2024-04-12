d3e7cc00063d7500fa3acb3af131e4b9039d461a
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-12 11:02:25 +0530

    Time: 13 ms (35.99%), Space: 22.2 MB (95.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
-        prefix[0]=height[0];
-        suffix[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            prefix[i] = max(prefix[i-1],height[i]);
+        int n=height.size();
+        int l=0;
+        int r= n-1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int ans = 0;
+        while(l<=r){
+            if(height[l]<height[r]){
+                if(height[l]>leftmax){
+                    leftmax = height[l];
+                }
+                else{
+                    ans+= leftmax - height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r]>rightmax){
+                    rightmax = height[r];
+                }
+                else{
+                    ans+= rightmax - height[r];
+                }
+                r--;
+            }
         }
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = max(suffix[i+1],height[i]);
-        }
-        for(int i=0;i<n;i++){
-            cout<<prefix[i];
-        }
-        cout<<endl;
-        for(int i=0;i<n;i++){
-            cout<<suffix[i];
-        }
-        cout<<endl;
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum = sum + (min(prefix[i],suffix[i]) - height[i]);
-        }
-        return sum;
+        return ans;
     }
 };
