7cb2de87fffc21e765623163f36977209400d99a
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-12 14:48:37 +0530

    Time: 3 ms (70.64%), Space: 12.2 MB (7.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,21 +4,17 @@ public:
         int low=0;
         int high=nums.size()-1;
         int mid;
-        if(target<=nums[0]){
-            return 0;
-        }
+        int ans = nums.size();
         while(low<=high){
             mid =(high+low)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
+            if(nums[mid]>=target){
+                ans = mid;
                 high = mid-1;
             }
+            else{
+                low = mid+1;
+            }
         }
-        return low;
+        return ans;
     }
 };
