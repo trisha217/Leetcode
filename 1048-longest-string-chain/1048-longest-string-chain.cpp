c00620dacb9acf1acff4a95867e6fa537e163f87
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-12 19:05:15 +0530

    Time: 60 ms (58.00%), Space: 16.6 MB (98.21%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-    bool compare(string& s1, string& s2){
-    if(s1.size() != s2.size() + 1) return false;
-    
-    int first = 0;
-    int second = 0;
-    
-    while(first < s1.size()){
-        if(second < s2.size() && s1[first] == s2[second]){
-            first ++;
-            second ++;
+    bool check(string &a, string &b){
+        int m = a.size();
+        int n = b.size();
+        if(m!=n+1){
+            return false;
         }
-        else first ++;
-    }
-    if(first == s1.size() && second == s2.size()) return true;
-    else return false; 
-}
-
-bool static comp(string& s1, string& s2){
-    return s1.size() < s2.size();
-}
-    int longestStrChain(vector<string>& arr) {
-        int n = arr.size();
-    
-    //sort the array
-    
-    sort(arr.begin(), arr.end(),comp);
-
-    vector<int> dp(n,1);
-    
-    int maxi = 1;
-    
-    for(int i=0; i<=n-1; i++){
-        
-        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-            
-            if( compare(arr[i], arr[prev_index]) && 1 + dp[prev_index] > dp[i]){
-                dp[i] = 1 + dp[prev_index];
+        int first =0;
+        int second =0;
+        while(first<m){
+            if(a[first]==b[second]){
+                first++;
+                second++;
+            }
+            else{
+                first++;
             }
         }
-        
-        if(dp[i] > maxi)
-            maxi = dp[i];
+        if(first == m && second ==n){
+            return true;
+        }
+        return false;
     }
-    return maxi;
+
+    bool static comp(string& s1, string& s2){
+        return s1.size() < s2.size();
+    }
+
+    int longestStrChain(vector<string> &arr){
+        int n=arr.size();
+        sort(arr.begin(),arr.end(),comp);
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(check(arr[i],arr[j])){
+                    dp[i] = max(1+dp[j],dp[i]);
+                }
+            }
+            if(dp[i]>maxi){
+                maxi = max(maxi,dp[i]);
+            }
+        }
+        return maxi;
     }
 };
+
