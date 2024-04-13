9c9100477d0d4bd2a8e2b3f66ea2baf8a3fedaa5
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-14 01:51:10 +0530

    Time: 6 ms (46.15%), Space: 17.7 MB (18.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<vector<int>> &dp,vector<int>& prices,int n){
-        //base case
-        if (ind == n) {
-            return 0;
-        }
-        // If the result for this state has already been calculated, return it
-        if (dp[ind][buy] != -1) {
-            return dp[ind][buy];
-        }
+    int maxProfit(vector<int>& Arr) {
+        int n = Arr.size();
+        // Create a DP table to memoize results
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
 
-        int profit = 0;
-        if (buy == 0) { // We can buy the stock
-            profit = max(0 + f(ind + 1,0,dp,prices,n), -prices[ind] + f(ind + 1, 1,dp,prices,n));
-        }
+        // Base condition: When we have no stocks left, the profit is 0.
+        dp[n][0] = dp[n][1] = 0;
 
-        if (buy == 1) { // We can sell the stock
-            profit = max(0 + f(ind + 1,1,dp,prices,n), prices[ind] + f(ind + 1, 0,dp,prices,n));
-        }
+        int profit;
 
-        // Store the calculated profit in the DP table and return it
-        return dp[ind][buy] = profit;
-        
-    }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        if (n == 0) {
-            return 0;
+        // Loop through the array in reverse order
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 0) { // We can buy the stock
+                    profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
         }
 
-        int ans = f(0,0,dp,prices,n);
-        return ans;
-
+        // The maximum profit is stored in dp[0][0] after all calculations
+        return dp[0][0];
     }
 };
