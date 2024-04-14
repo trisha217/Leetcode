a0e0e8983b37ee83e799799e11bf3b5f7b2543f3
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-14 17:01:11 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (48.78%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,21 +4,11 @@ public:
         int n = Arr.size();
         // Create a DP table to memoize results
         vector<vector<int>> dp(n + 2, vector<int>(2, 0));
-
-        int profit;
-
+        
         // Loop through the array in reverse order
         for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy) { // We can buy the stock
-                    profit = max(0 + dp[ind + 1][1], -Arr[ind] + dp[ind + 1][0]);
-                }
-                else { // We can sell the stock
-                    profit = max(0 + dp[ind + 1][0], Arr[ind] + dp[ind + 2][1]);
-                }
-
-                dp[ind][buy] = profit;
-            }
+            dp[ind][1] = max(0 + dp[ind + 1][1], -Arr[ind] + dp[ind + 1][0]);
+            dp[ind][0] = max(0 + dp[ind + 1][0], Arr[ind] + dp[ind + 2][1]);
         }
 
         // The maximum profit is stored in dp[0][1] after all calculations
