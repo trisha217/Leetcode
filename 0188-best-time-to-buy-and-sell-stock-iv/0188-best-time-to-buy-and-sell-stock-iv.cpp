968bef2396138cfa1f2374352ac3b91cae9b4b8e
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-14 14:51:53 +0530

    Time: 7 ms (70.63%), Space: 12.9 MB (86.08%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& Arr) {
-        int n=Arr.size();
-        // Creating a 3d - dp of size [n+1][2][k+1] initialized to 0
-    vector<vector<vector<int>>> dp(n+1,
-                                    vector<vector<int>> 
-                                            (2,vector<int>(k+1,0)));
-                                            
-    // As dp array is initialized to 0, we have already covered the base case
-    
-    for(int ind = n-1; ind>=0; ind--){
-        for(int buy = 0; buy<=1; buy++){
-            for(int cap=1; cap<=k; cap++){
-                
-                if(buy==0){// We can buy the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
-                                -Arr[ind] + dp[ind+1][1][cap]);
-                 }
-    
-                if(buy==1){// We can sell the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
-                                Arr[ind] + dp[ind+1][0][cap-1]);
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        int profit;
+        vector<vector<int>> ahead(2,vector<int> (k+1,0));
+        vector<vector<int>> cur(2,vector<int> (k+1,0));
+        //recurrence
+        for(int ind = n-1;ind>=0;ind--)
+        {
+            for(int buy =0;buy<=1;buy++)
+            {
+                for(int cap =1;cap<=k;cap++)
+                {
+                    if(buy){
+                        cur[buy][cap] = max(-prices[ind] + ahead[0][cap], 0 + ahead[1][cap]);
+                    }                    
+                    else{
+                           cur[buy][cap] = max(prices[ind] + ahead[1][cap-1], 0 + ahead[0][cap]);
+                    }
                 }
             }
+            ahead = cur;
         }
-    }
-    
-    
-    return dp[0][0][k];
+        return ahead[1][k];
     }
 };
