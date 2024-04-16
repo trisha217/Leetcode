74c280586df7724f7c99d304c6a88d593376e32d
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-16 16:17:02 +0530

    Time: 20 ms (51.12%), Space: 31.3 MB (41.44%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2,string x, string y, vector<vector<int>> &dp){
-        if(ind2<0){
-            return 1;
-        }
-        if(ind1<0){
-            return 0;
-        }
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-        if(x[ind1]==y[ind2]){
-            return dp[ind1][ind2] = solve(ind1-1,ind2-1,x,y,dp) + solve(ind1-1,ind2,x,y,dp);
-
-        }
-        return dp[ind1][ind2] = solve(ind1-1,ind2,x,y,dp);
-    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(n-1,m-1,s,t,dp);
+        int MOD = 1e9 + 7; // Define a modulo value
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0)); // Changed int to long long
+        for (int ind1 = 0; ind1 <= n; ind1++) {
+            dp[ind1][0] = 1;
+        }
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            dp[0][ind2] = 0;
+        }
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s[ind1 - 1] == t[ind2 - 1]) {
+                    dp[ind1][ind2] = (dp[ind1 - 1][ind2 - 1] + dp[ind1 - 1][ind2])% MOD;
+                } else {
+                    dp[ind1][ind2] = dp[ind1 - 1][ind2];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
