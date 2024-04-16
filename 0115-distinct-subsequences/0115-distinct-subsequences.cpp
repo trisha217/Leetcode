ce3d0d4b45c6e909deb1802078b75af698d94f40
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-16 16:36:49 +0530

    Time: 9 ms (89.34%), Space: 7.5 MB (99.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -5,17 +5,13 @@ public:
         int m = t.size();
         int MOD = 1e9 + 7; // Define a modulo value
         vector<int> prev(m + 1, 0); 
-        vector<int> cur(m + 1, 0); 
-        prev[0]=cur[0]=1;
+        prev[0]=1;
         for (int ind1 = 1; ind1 <= n; ind1++) {
-            for (int ind2 = 1; ind2 <= m; ind2++) {
+            for (int ind2 = m; ind2 >= 1; ind2--) {
                 if (s[ind1 - 1] == t[ind2 - 1]) {
-                    cur[ind2] = (prev[ind2 - 1] + prev[ind2])% MOD;
-                } else {
-                    cur[ind2] = prev[ind2];
-                }
+                    prev[ind2] = (prev[ind2 - 1] + prev[ind2])% MOD;
+                } 
             }
-            prev = cur;
         }
         return prev[m];
     }
