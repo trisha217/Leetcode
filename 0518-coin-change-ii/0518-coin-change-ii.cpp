d8387d711e4fc2457f69a65ebc29667141aa0f69
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-21 00:03:53 +0530

    Time: 14 ms (40.13%), Space: 8.7 MB (77.67%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
-    int change(int t, vector<int>& cs) {
-  int dp[5001] = { 1 };
-  for (auto c : cs)
-    for (auto j = c; j <= t; ++j) dp[j] += dp[j - c];
-  return dp[t];
-}
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> prev(amount+1,0);
+        vector<int> cur(amount+1,0);
+        //base case
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0] == 0){
+                prev[i] = 1;
+            }
+            else{
+                prev[i] = 0;
+            }
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int tar =0;tar<=amount;tar++){
+                int nottake = 0 + prev[tar];
+                int take = 0;
+                if(coins[ind]<=tar){
+                    take = cur[tar-coins[ind]];
+                }
+                cur[tar] = take + nottake;
+            }
+            prev = cur;
+        }
+        int ans =  prev[amount];
+        return ans;
+    }
 };
