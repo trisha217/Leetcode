43bc8f8c3218a2cbc7177cdb89830c8b499c504f
Trisha Moitra <trishamoitra.217@gmail.com>
2024-04-24 01:16:49 +0530

    Time: 1498 ms (43.55%), Space: 7.5 MB (95.11%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -25,7 +25,17 @@ public:
     }
     int minCut(string str) {
         int n = str.size();
-        vector<int> dp(n, -1);
-        return minPartitions(0, n, str, dp) - 1;
+        vector<int> dp(n+1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = INT_MAX;
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j, str)) {
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0]-1;
     }
 };
