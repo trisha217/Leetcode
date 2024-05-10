666a4d7e3b120a7f6a56e8311db72fa7390c4aeb
Trisha Moitra <trishamoitra.217@gmail.com>
2024-05-11 00:20:54 +0530

    Time: 3 ms (63.89%), Space: 13.2 MB (62.51%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -5,7 +5,10 @@ public:
         ListNode* fast = head;
         
 		// move fast pointer to the n + 1 element
-        while (n--) { fast = fast->next; }
+        for(int i = 0; i < n; i++)
+        { 
+            fast = fast->next; 
+        }
         
         // handle edge case: given n is always valid, 
         // if fast reached the end, we need to remove the first element
@@ -17,9 +20,10 @@ public:
             slow = slow->next;
         }
         
+        ListNode* delNode = slow->next;
         // slow will be pointing to the element before the one we want to remove
         slow->next = slow->next->next;
-        
+        delete(delNode);
         return head;
     }
 };
