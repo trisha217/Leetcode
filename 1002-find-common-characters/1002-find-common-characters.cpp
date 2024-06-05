83447ecbf538601c5ffad63855b6401958cb792b
Trisha Moitra <trishamoitra.217@gmail.com>
2024-06-05 09:22:35 +0530

    Time: 4 ms (84.24%), Space: 11 MB (90.73%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        char letters[26] = {};
+        for(auto c: words[0]) {
+            letters[c - 'a']++;
+        }
+
+        char aux[26] = {};
+        for(int i = 1; i < words.size(); i++) {
+            for(auto c:words[i]) {
+                if(letters[c - 'a'] == 0)
+                    continue;
+                
+                aux[c - 'a']++;
+            }
+
+            for(int i = 0; i < 26; i++) {
+                letters[i] = min(aux[i], letters[i]);
+                aux[i] = 0;
+            }
+        }
+
+        vector<string> output;
+        for(int i = 0; i < 26; i++) {
+            while(letters[i]) {
+                string str;
+                str += i + 'a';
+                output.push_back(str);
+                letters[i]--;
+            }
+        }
+
+        return output;
+    }
+};
