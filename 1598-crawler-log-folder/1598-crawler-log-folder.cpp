ee59b71b7e77978a785eb10a44649bf93105c4f5
Trisha Moitra <trishamoitra.217@gmail.com>
2024-07-10 10:04:59 +0530

    Time: 7 ms (26.85%), Space: 14.4 MB (17.13%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        int n=logs.size();
+        for(int i=0;i<n;i++){
+            if(logs[i][0] != '.'){
+                st.push(logs[i]);
+                continue;
+            }
+            if(!st.empty() && (logs[i] == "../")){
+                st.pop();
+            }
+        }
+        return st.size();
+    }
+};
