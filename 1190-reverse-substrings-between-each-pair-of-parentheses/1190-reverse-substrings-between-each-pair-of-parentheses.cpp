cb8c887fcdf10a8ffcd9a97c0f2178486c75775a
Trisha Moitra <trishamoitra.217@gmail.com>
2024-07-12 00:22:15 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (37.54%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<char> mystack;
+        for(char c : s) {
+            if(c == ')') {
+                func(mystack);
+            } else {
+                mystack.push(c);
+            }
+        }
+        string res = "";
+        while(!mystack.empty()) {
+            res += mystack.top();
+            mystack.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    void func(stack<char> &myStack) {
+        string t = "";
+        while(!myStack.empty() && myStack.top() != '(') {
+            t += myStack.top();
+            myStack.pop();
+        }
+        if(!myStack.empty())
+            myStack.pop();
+        for(char c : t)
+            myStack.push(c);
+    }
+};
+
+
+//(u(love)i)
+//(uevoli)
+//iloveu
