541584cdccee7222d9d9faff9db6588b0e26c8ed
Trisha Moitra <trishamoitra.217@gmail.com>
2024-07-31 01:02:35 +0530

    Time: 27 ms (69.69%), Space: 16.1 MB (68.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid, int y, int x){
+        if(x<0 ||y<0 || y>=grid.size() || x>=grid[0].size() || grid[y][x]!='1'){
+            return;
+        }
+        grid[y][x] = '*';
+        dfs(grid,y-1,x);
+        dfs(grid,y+1,x);
+        dfs(grid,y,x-1);
+        dfs(grid,y,x+1);
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int ans =0;
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     ans++;
                     dfs(grid,i,j);
@@ -11,16 +23,5 @@ public:
             }
         }
         return ans;
-    }
-    void dfs(vector<vector<char>>& grid,int y, int x){
-        if(x<0 || x>=grid[0].size() || y<0 || y>=grid.size() || grid[y][x]!='1'){
-            return;
-        }
-        grid[y][x] = '*'; // destroying visited cells so they are not re-visited
-        // recursive dfs on all neighbours
-        dfs(grid, y + 1, x);
-        dfs(grid, y - 1, x);            
-        dfs(grid, y, x + 1);
-        dfs(grid, y, x - 1);
     }
 };
