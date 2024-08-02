deb58e4222a3e65731222a9f8e64e35fabd4c16e
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-02 09:08:20 +0530

    Time: 13 ms (90.18%), Space: 16.8 MB (21.95%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-int bfs(int sr,vector<vector<int>>& adj,vector<int> &vis)
-{
-    vis[sr]=1;
-    queue<pair<int,int>> q;
-    map<int,int> mp;
-    q.push({sr,-1});
-    mp[sr]=0;
-    while(q.size()>0)
-    {
-        int a=q.front().first,b=q.front().second;
-        q.pop();
-        for(int i=0;i<adj[a].size();i++)
-        {
-            if(vis[adj[a][i]]==0)
-            {
-                vis[adj[a][i]]=1;
-                mp[adj[a][i]]=mp[a]+1;
-                q.push({adj[a][i],a});
-            }
-            else if(adj[a][i]!=b)
-            {
-                //cout<<adj[a][i]<<sr<<endl;
-                if((mp[a]+mp[adj[a][i]])%2==0)
-                return 0;
+    bool isBipartite(vector<vector<int>>& graph) {
+        queue<int> q;
+        int n = graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                queue<int> q;
+                q.push(i);
+                color[i] = 0;
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for(auto it: graph[node]){
+                        if(color[it]==-1){
+                            color[it] = !color[node];
+                            q.push(it);
+                        }
+                        else if(color[it] == color[node]){
+                            return false;
+                        }
+                    }
+                }
             }
         }
+        return true;
     }
-    return 1;
-}
-	bool isBipartite(vector<vector<int>>& graph){
-	    int i,j,p,V=graph.size();
-	    vector<int> vis(V,0);
-	    for(i=0;i<V;i++)
-	    {
-	        if(vis[i]==0)
-	        p=bfs(i,graph,vis);
-	        if(p==0)
-	        return false;
-	    }
-	    return true;
-	}
 };
