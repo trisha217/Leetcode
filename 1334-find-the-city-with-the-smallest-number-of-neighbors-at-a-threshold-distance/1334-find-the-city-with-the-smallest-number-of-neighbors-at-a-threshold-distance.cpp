857d736884d0d521688855c2d78e5b568884905e
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-07 17:44:53 +0530

    Time: 29 ms (46.96%), Space: 15.8 MB (52.09%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,52 +1,36 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // Initialize distance matrix with infinity
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        
-        // Set diagonal elements to 0
-        for (int i = 0; i < n; i++) {
-            dist[i][i] = 0;
+        int m = edges[0].size();
+        vector<vector<int>> dist(n,vector<int> (n,1e9));
+        for(auto it: edges){
+            dist[it[0]][it[1]] = it[2];
+            dist[it[1]][it[0]] = it[2];
         }
-        
-        // Fill the distance matrix with given edges
-        for (const auto& edge : edges) {
-            int from = edge[0], to = edge[1], weight = edge[2];
-            dist[from][to] = weight;
-            dist[to][from] = weight;
-        }
-        
-        // Floyd-Warshall algorithm
+        for (int i = 0; i < n; i++) dist[i][i] = 0;
+        //floyd warshall
         for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && 
-                        dist[i][k] + dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-        }
-        
-        int minReachableCities = INT_MAX;
-        int resultCity = -1;
-        
-        // Count reachable cities for each city
-        for (int i = 0; i < n; i++) {
-            int reachableCities = 0;
-            for (int j = 0; j < n; j++) {
-                if (i != j && dist[i][j] <= distanceThreshold) {
-                    reachableCities++;
-                }
-            }
-            
-            // Update result if current city has fewer or equal reachable cities
-            if (reachableCities <= minReachableCities) {
-                minReachableCities = reachableCities;
-                resultCity = i;
-            }
-        }
-        
-        return resultCity;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] == 1e9 || dist[k][j] == 1e9)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+        int cntcity = n;
+        int cityno = -1;
+        for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
+			if (cnt <= cntcity) {
+				cntcity = cnt;
+				cityno = city;
+			}
+		}
+        return cityno;
     }
 };
