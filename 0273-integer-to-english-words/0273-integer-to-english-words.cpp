7478fac7abbdf748176f77c0daa20c693de7e076
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-08 01:10:15 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (82.07%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string numberToWords(int num) {
+        string belowTen[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+
+        string belowTwenty[]={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+        string belowHundred[]={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+
+        if(num==0)
+        return "Zero";
+
+        if( num<10)
+        return belowTen[num];
+
+        if(num<20)
+        return belowTwenty[num-10];
+
+          if(num<100){//74, 70
+            return belowHundred[num/10] + (num%10!=0?" " + belowTen[num%10]:"");
+        }
+        if(num<1000){ //9 00
+            return belowTen[num/100] + " Hundred" + (num%100!=0?" " + numberToWords(num%100):"");
+        }
+        if(num<1000000){ //9000 -- 999 000
+            return numberToWords(num/1000) + " Thousand" + (num%1000!=0?" " + numberToWords(num%1000):"");
+        }
+        if(num<1000000000){ //999 129786
+            return numberToWords(num/1000000) + " Million" + (num%1000000!=0?" " + numberToWords(num%1000000):"");
+        }
+        return numberToWords(num/1000000000) + " Billion" + (num%1000000000!=0?" " + numberToWords(num%1000000000):"");
+    }
+};
