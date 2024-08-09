d7d1a4c33bc506f572bad52439e3832ed0a88a4f
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-09 16:03:18 +0530

    Time: 4 ms (56.00%), Space: 14.8 MB (26.20%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
-        int n=arr.size();
-        next_permutation(arr.begin(),arr.end());
-        //using in-built function of C++
-        for(int i=0;i<n;i++){
-            cout<<arr[i];
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int dip=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                dip = i;
+                break;
+            }
         }
-        return;
+        if(dip==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for (int i = n - 1; i > dip; i--) 
+        {
+            if (nums[i] > nums[dip]) 
+            {
+                swap(nums[i], nums[dip]);
+                break;
+            }
+        }
+        // Step 3: reverse the right half:
+        reverse(nums.begin() + dip + 1, nums.end());
     }
 };
