8d8d4677014fbb0240c31d6c0df03bfaddfa6f97
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-16 17:30:57 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (47.66%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>& arr){
-        int n = arr.size();
-        int prev = arr[0];
-        int prev2 =0;
-    
-        for(int i=1; i<n; i++){
-            int pick = arr[i];
+    int solve(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0; 
+        int cur, prev=nums[0], prev2=nums[0];
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
             if(i>1)
-                pick += prev2;
-            int nonPick = 0 + prev;
-        
-            int cur_i = max(pick, nonPick);
+                pick = pick + prev2;
+            int notpick = 0 + prev;
+            cur= max(pick,notpick);
             prev2 = prev;
-            prev= cur_i;
-        
+            prev = cur;
         }
         return prev;
     }
@@ -22,18 +19,17 @@ public:
         vector<int> arr1;
         vector<int> arr2;
         int n = nums.size();
-        if(n==1)
-            return nums[0];
-    
-        for(int i=0; i<n; i++){
-        
-            if(i!=0) arr1.push_back(nums[i]);
-            if(i!=n-1) arr2.push_back(nums[i]);
+        if (n == 1) return nums[0]; // Handle single element case
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                arr1.push_back(nums[i]);
+            }
+            if(i!=n-1){
+                arr2.push_back(nums[i]);
+            }
         }
-    
         int ans1 = solve(arr1);
         int ans2 = solve(arr2);
-    
         return max(ans1,ans2);
     }
 };
