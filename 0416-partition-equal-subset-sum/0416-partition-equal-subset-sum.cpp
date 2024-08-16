473292f7f03b23724ef740017faccd14db2e7a32
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-16 19:38:51 +0530

    Time: 273 ms (30.15%), Space: 13 MB (89.98%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& arr) {
+        int sum = 0;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            sum = sum + arr[i];
+        }
+        if(sum%2==1){
+            return false;
+        }
+        sum = sum/2;
+        vector<bool> prev(sum + 1, false);
+        vector<bool> cur(sum + 1, false);
+        prev[0]= cur[0] = true;
+        if(arr[0] <= sum)
+            prev[arr[0]] = true;
+        for(int ind = 1; ind < n; ind++) {
+            for(int target = 1; target <= sum; target++) {
+                bool pick = false;
+                bool notpick;
+                if(arr[ind] <= target) {
+                    pick = prev[target - arr[ind]];
+                }
+                notpick = prev[target];
+                cur[target] = notpick || pick;
+            }
+            prev = cur;
+        }
+        return prev[sum];
+    }
+};
