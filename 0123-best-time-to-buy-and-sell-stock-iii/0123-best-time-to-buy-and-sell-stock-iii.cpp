d8b887eae3be247dec95b64619ff0bf47a13ac88
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-17 17:18:56 +0530

    Time: 459 ms (36.71%), Space: 210 MB (41.71%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    // int f(int ind, int buy, int cap, vector<int> &prices,vector<vector<vector<int>>> &dp){
-    //     int profit;
-    //     //base case
-    //     if(ind == prices.size() || cap == 0){
-    //         return 0;
-    //     }
-    //     //if value is already computed
-    //     if(dp[ind][buy][cap]!=-1){
-    //         return dp[ind][buy][cap];
-    //     }
-    //     if(buy){
-    //         profit = max(-prices[ind] + f(ind+1,0,cap,prices,dp), 0 + f(ind+1,1,cap,prices,dp));
-    //     }
-    //     else{
-    //         profit = max(prices[ind] + f(ind+1,1,cap-1,prices,dp), 0 + f(ind+1,0,cap,prices,dp));
-    //     }
-    //     return dp[ind][buy][cap] = profit;
-    // }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int profit;
-        vector<vector<int>> ahead(2,vector<int> (3,0));
-        vector<vector<int>> cur(2,vector<int> (3,0));
-        //recurrence
-        for(int ind = n-1;ind>=0;ind--){
-            for(int buy =0;buy<=1;buy++){
-                for(int cap =1;cap<=2;cap++){
-                    if(buy){
-                        cur[buy][cap] = max(-prices[ind] + ahead[0][cap], 0 + ahead[1][cap]);
-                    }                    
-                    else{
-                           cur[buy][cap] = max(prices[ind] + ahead[1][cap-1], 0 + ahead[0][cap]);
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        //base case: if( ind == n || cap == 0) return 0
+        //no need of writing separately, because we have initiated eveyrthing to 0.
+        long profit=0;
+        //n days
+        for(int i=n-1;i>=0;i--){
+            //buy
+            for(int j=0;j<2;j++){
+                //cap - transactions
+                for (int cap = 1; cap <= 2; cap++){
+                   if(j==0){
+                        int op1 = 0 + dp[i+1][0][cap];
+                        int op2 = -prices[i] + dp[i+1][1][cap];
+                        profit = max(op1,op2);
                     }
+                    else{
+                        int op1 = 0 + dp[i+1][1][cap];
+                        int op2 = prices[i] + dp[i+1][0][cap-1];
+                        profit = max(op1,op2);
+                    }
+                    dp[i][j][cap] = profit; 
                 }
             }
-            ahead = cur;
         }
-        return ahead[1][2];
-        //return f(0,1,2,prices,dp);
+        return dp[0][0][2];
     }
 };
