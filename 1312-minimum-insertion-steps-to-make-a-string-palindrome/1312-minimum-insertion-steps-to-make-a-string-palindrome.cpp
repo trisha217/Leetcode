00ee0b8f1793360d2c4d4bb7f0c1680dd0736a44
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-18 00:20:02 +0530

    Time: 16 ms (95.34%), Space: 9 MB (79.90%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int minInsertions(string x) {
         string y = x;
-        reverse(x.begin(),x.end());
+        reverse(y.begin(),y.end());
         int n=x.size(),m=y.size();
-        vector<int> cur(m+1,0);
-        vector<int> prev(m+1,0);
-        int ans = 0;
+        int dp[n+1][m+1];
+        //base case ( i==0 || j==0) return 0;
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i]=0;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(x[i-1]==y[j-1])
-                    cur[j]=1 + prev[j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 else {
-                    cur[j]=max(cur[j-1],prev[j]);
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
             }
-            prev = cur;
         }
-        return n-prev[m];
+        return n - dp[n][m];
     }
 };
