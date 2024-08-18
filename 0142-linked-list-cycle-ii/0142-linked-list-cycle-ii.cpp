d71ed7577b4b3e6c1cfa294bc89f4645721719a8
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-18 16:08:24 +0530

    Time: 9 ms (49.99%), Space: 10.4 MB (28.82%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -13,13 +13,15 @@ public:
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) break;
+            if (slow == fast){
+                slow = head;        
+                while (fast != slow) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
-        if (!(fast && fast->next)) return NULL;
-        while (head != slow) {
-            head = head->next;
-            slow = slow->next;
-        }
-        return head;
+        return NULL;
     }
 };
