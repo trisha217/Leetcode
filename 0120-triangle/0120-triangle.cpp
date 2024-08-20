734815e3d24c2e1b8a05449b7ad5ad37fb605980
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-20 23:05:47 +0530

    Time: 8 ms (27.63%), Space: 11.5 MB (49.21%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // Initialize the bottom row of dp with the values from the triangle
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle[n - 1][j];
+        }
+        // Iterate through the triangle rows in reverse order
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                // Calculate the minimum path sum for the current cell
+                int down = triangle[i][j] + dp[i + 1][j];
+                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+
+                // Store the minimum of the two possible paths in dp
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        return dp[0][0]; 
+    }
+};
