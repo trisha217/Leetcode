e8ebce5de4a42f794fb8b903aac526156cb86f48
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-25 02:21:28 +0530

    Time: 203 ms (27.34%), Space: 67.4 MB (12.04%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,31 +1,22 @@
 class Solution {
-private:
-    int atmostk(vector<int>& nums, int k){
-        int left = 0;
-        int right = 0;
-        int count = 0;
-        int n = nums.size();
-        int freq[20005] = {0};
-        
-        while(right < n){
-            if(freq[nums[right]]++ == 0){
-                k--;
-            }
-            while(k < 0){
-                freq[nums[left]]--;
-                if(freq[nums[left]] == 0) k++;
-                left++;
-            }
-
-            count += right - left + 1;
-
-            right++;
-        }
-        return count;
-    }
 public:
+    int solve(vector<int>& nums, int k){
+        int l=0,r=0,cnt=0;
+        map<int,int> mpp;
+        while(r<nums.size()){
+            mpp[nums[r]]++;
+            while(mpp.size() > k){
+                mpp[nums[l]]--;
+                if (mpp[nums[l]] == 0)
+                    mpp.erase(nums[l]);
+                l++;
+            }
+            cnt = cnt + (r - l + 1);
+            r++;
+        }
+        return cnt;
+    }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        
-        return atmostk(nums,k) - atmostk(nums,k-1);
+        return solve(nums,k) - solve(nums,k-1);
     }
 };
