7e4dd36ae873be4b959f04ca17d34a1520f2a9f8
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-27 10:10:38 +0530

    Time: 269 ms (68.19%), Space: 116.6 MB (55.58%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    unordered_map<int, vector<pair<int, bool>>> G;
-    bool visited[(int) 1e5 + 1];
-    int solve(int node) {
-        if(visited[node]) {
-            return 0;
-        }
-        visited[node] = true;
-        int ans = 0;
-        for(auto &edge: G[node]) {
-            int child = edge.first;
-            int direction = edge.second;
-            if(direction == false && !visited[child]) {
-                // cout << node << " - " << child << " - " <<  direction << endl;
-                ans += 1 + solve(child);
-            } else {
-                ans += solve(child);
+    void dfs(vector<vector<pair<int, int>>>& adj, vector<int>& vis, int node,
+             int& orient) {
+        vis[node] = 1;
+        for (auto [nei, direction] : adj[node]) {
+            if (!vis[nei]) {
+                // If direction is 1, it means the edge was originally directed
+                // from node to neighbor
+                //but we need to flip it(neighbour to node)
+                if (direction == 1) {
+                    orient++;
+                }
+                dfs(adj, vis, nei, orient);
             }
         }
-        return ans;
     }
-
     int minReorder(int n, vector<vector<int>>& connections) {
-        for(auto &connection : connections) {
-            G[connection[0]].push_back({connection[1], false}); // away false;
-            G[connection[1]].push_back({connection[0], true}); // towards true
+        int ans =0;
+        vector<int> vis(n,0);
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto it: connections){
+            adj[it[0]].emplace_back(it[1], 1); // 1 means original direction
+            adj[it[1]].emplace_back(it[0], 0); 
         }
-        memset(visited, false, sizeof(visited));
-        return solve(0);
+        dfs(adj, vis, 0, ans);
+        return ans;
     }
 };
