6e7e4176ad02d420feb388773cf7737ef59e77be
Trisha Moitra <trishamoitra.217@gmail.com>
2024-08-27 12:07:05 +0530

    Time: 23 ms (88.64%), Space: 16 MB (80.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int y, int x){
+        //if out of bounds
         if(x<0 ||y<0 || y>=grid.size() || x>=grid[0].size() || grid[y][x]!='1'){
             return;
         }
+        //marking the grid so we don't visit again
         grid[y][x] = '*';
         dfs(grid,y-1,x);
         dfs(grid,y+1,x);
