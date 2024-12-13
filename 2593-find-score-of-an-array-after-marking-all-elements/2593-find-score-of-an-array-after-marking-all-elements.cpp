88c6046e184e10c943773390b37ab2ee4acfd32a
Trisha Moitra <trishamoitra.217@gmail.com>
2024-12-14 00:19:37 +0530

    Time: 111 ms (74.73%), Space: 105.7 MB (57.19%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    class CustomNode {
+        public:
+        int val;
+        int idx;
+        CustomNode(int val, int idx) {
+            this->val = val;
+            this->idx = idx;
+        }
+        
+        bool operator < (const CustomNode& otherNode) const {
+            if(val == otherNode.val) return idx > otherNode.idx;
+            return val > otherNode.val;
+        }
+    };
+    
+    
+public:
+    typedef long long ll;
+    long long findScore(vector<int>& nums) {
+        priority_queue<CustomNode> pq;
+        int n = nums.size();
+        ll res = 0;
+        for(int i=0;i<n;i++) {
+            pq.push(CustomNode(nums[i], i));
+        }
+        vector<bool> vis(n);
+        while(pq.size()) {
+            CustomNode currnode = pq.top(); pq.pop();
+            if(vis[currnode.idx]) continue;
+            vis[currnode.idx] = 1;
+            if(currnode.idx > 0) vis[currnode.idx - 1] = 1;
+            if(currnode.idx < n - 1) vis[currnode.idx + 1] = 1;
+            res += currnode.val;
+        }
+        return res;
+        
+    }
+};
